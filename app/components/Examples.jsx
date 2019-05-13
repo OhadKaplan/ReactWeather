ce5e938a72fb927d9b53cab6da54cf616ff6d585
Ohad Kaplan <mail@mail.com>
2019-05-13 06:02:11 +0300

    Updated Copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -15,7 +15,10 @@ var React = require('react');
 
 var Examples = (props)=>{
             return(
-                <h3>Examples Component!!</h3>      
+                <div>
+                    <h3>Examples Component!!</h3>  
+                    <p>Welcome to GitHub</p>
+                </div>
             )
 }
 module.exports = Examples;
