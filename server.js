8878a7ee17de0f45b91f1ad532a9fcfa48ed3d14
Ohad Kaplan <mail@mail.com>
2019-05-13 17:22:34 +0300

    prep repo for Hiroku

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ var express = require('express');
 //creat our app
 
 var app = express();
+const PORT=process.env.PORT || 3000;
+
+app.use(function(req,res,next){
+    if(req.headers["x-forwarded-proto"]==='http'){
+        next();
+    }else{
+        req.redirect('https://'+req.hostname+req.url);
+    }
+})
 
 app.use(express.static('public'));
 
-app.listen(3000, function (){
-    console.log("Express Server Is Up On Port 3000");
+app.listen(PORT, function (){
+    console.log('Express Server Is Up On Port'+PORT);
 });
